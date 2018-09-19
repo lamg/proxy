4fa39ca9bfc34ff8b7d8b71ce624d9a84eac5133
Luis Ángel Méndez Gort <gort.andres000@gmail.com>
2018-09-19 14:10:47 -0400

    Test request is sent in context

--- proxy_test.go
+++ proxy_test.go
@@ -1,9 +1,35 @@
 package proxy
 
 import (
+	"context"
+	"net"
+	h "net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestProxy(t *testing.T) {
+	d := &dialer{t: t}
+	p := &Proxy{
+		Tr: &h.Transport{
+			DialContext: d.DialContext,
+		},
+	}
+	r := httptest.NewRequest(h.MethodGet, "http://bla.com", nil)
+	w := httptest.NewRecorder()
+	p.handleTunneling(w, r)
+}
 
+type dialer struct {
+	t *testing.T
+}
+
+func (d *dialer) DialContext(ctx context.Context, nt,
+	host string) (c net.Conn, e error) {
+	r, ok := ctx.Value(ReqKey).(*h.Request)
+	require.True(d.t, ok)
+	require.Equal(d.t, "bla.com", r.Host)
+	return
 }
