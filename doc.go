e88200e4e15a7dae2b20d247e995b7fe8b51ea0a
Luis Ángel Méndez Gort <gort.andres000@gmail.com>
2019-02-07 11:47:56 -0500

    .Server

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 // proxy is an HTTP proxy that uses custom procedures
 // for network dialing and parent proxy selection
 // (HTTP or SOCKS5). It can be served using net/http.Server
-// or github.com/valyala/fasthttp
+// or github.com/valyala/fasthttp.Server
 package proxy
