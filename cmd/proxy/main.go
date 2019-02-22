62ae04e56c611fa383ad73c1afa027a409c01970
Luis Ángel Méndez Gort <gort.andres000@gmail.com>
2019-02-22 09:41:13 -0500

    copyright notice at each file, as recommended by FSF

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -1,3 +1,25 @@
+// Copyright © 2018-2019 Luis Ángel Méndez Gort
+
+// This file is part of Proxy.
+
+// Proxy is free software: you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your
+// option) any later version.
+
+// Proxy is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU Lesser General Public
+// License for more details.
+
+// You should have received a copy of the GNU Lesser General
+// Public License along with Proxy.  If not, see
+// <https://www.gnu.org/licenses/>.
+
+// proxy is an usage example of github.com/lamg/proxy
+// library
 package main
 
 import (
--- doc.go
+++ doc.go
@@ -1,4 +1,24 @@
-// proxy is an HTTP proxy that uses custom procedures
+// Copyright © 2018-2019 Luis Ángel Méndez Gort
+
+// This file is part of Proxy.
+
+// Proxy is free software: you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your
+// option) any later version.
+
+// Proxy is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU Lesser General Public
+// License for more details.
+
+// You should have received a copy of the GNU Lesser General
+// Public License along with Proxy.  If not, see
+// <https://www.gnu.org/licenses/>.
+
+// Proxy is an HTTP proxy that uses custom procedures
 // for network dialing and parent proxy selection
 // (HTTP or SOCKS5). It can be served using net/http.Server
 // or github.com/valyala/fasthttp.Server
